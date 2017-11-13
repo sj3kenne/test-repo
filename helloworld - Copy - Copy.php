40ff5fe804ac5d51a9a2ea15243b2e72f44122f7
sj3kenne <32079919+sj3kenne@users.noreply.github.com>
2017-11-13 12:21:29 -0500

    commit6

---
+++ helloworld - Copy - Copy.php
@@ -0,0 +1,16 @@
+<?php
+    print("<html>\n");
+    print("<head>\n");
+    print("<title> Test php page </title>\n");
+    print("</head>\n");
+    print("<body>\n");
+
+    for($i = 0; $i < 10; ++%i)
+    {
+        print("<p>" . ($i+1) . ". Hello!</p>\n");
+                print("<p>" . ($i+1) . ". Hello!</p>\n");
+    }
+
+    print("</body)\n");
+    print("</html>\n");
+?>
